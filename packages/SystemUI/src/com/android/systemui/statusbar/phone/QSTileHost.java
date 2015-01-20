3d2238572a30e13c1b95ba901e5b1e9199917b05
Roman Birg <roman@cyngn.com>
2015-01-19 16:22:56 -0800

    SystemUI: port compass tile to cm-12.0
    Change-Id: Ia1bf264ceb64b153f066c86ab6d2bad3d068b2ee
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -38,6 +38,7 @@ import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
+import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
@@ -284,6 +285,8 @@ public class QSTileHost implements QSTile.Host {
                 return new RoamingTile(this);
             case QSConstants.TILE_DDS:
                 return new DdsTile(this);
+            case QSConstants.TILE_COMPASS:
+                return new CompassTile(this);
             case QSConstants.TILE_APN:
                 return new ApnTile(this);
             case QSConstants.TILE_PROFILES:
