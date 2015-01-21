093bac667d3ee1fdcf5fc178af729d917c39b23a
Roman Birg <roman@cyngn.com>
2015-01-20 16:14:08 -0800

    SystemUI: port lock screen toggle tile
    Change-Id: Ibec9993f3bd339a2d48e00eea6442618a91b15c9
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
+import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.ProfilesTile;
 import com.android.systemui.qs.tiles.PerfProfileTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
@@ -297,6 +298,8 @@ public class QSTileHost implements QSTile.Host {
                 return new AdbOverNetworkTile(this);
             case QSConstants.TILE_NFC:
                 return new NfcTile(this);
+            case QSConstants.TILE_LOCKSCREEN:
+                return new LockscreenToggleTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
