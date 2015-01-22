b601f276779849e4b6d30636d1da2616eb24b73e
Roman Birg <roman@cyngn.com>
2015-01-21 18:59:20 -0800

    SystemUI: add NFC tile
    Change-Id: I884b3492b1d81c39235df1660e4e0e06653abce2
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -205,6 +205,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("screenshot")) return R.string.quick_settings_screenshot_label;
         else if (spec.equals("volume")) return R.string.quick_settings_volume_panel_label;
         else if (spec.equals("headsup")) return R.string.quick_settings_heads_up_label;
+        else if (spec.equals("nfc")) return R.string.quick_settings_nfc;
         return 0;
     }
 
@@ -453,6 +454,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("screenshot")) return R.drawable.ic_qs_screenshot;
             else if (mSpec.equals("volume")) return R.drawable.ic_qs_volume_panel;
             else if (mSpec.equals("headsup")) return R.drawable.ic_qs_heads_up_on;
+            else if (mSpec.equals("nfc")) return R.drawable.ic_qs_nfc_on;
             return R.drawable.android;
         }
 
