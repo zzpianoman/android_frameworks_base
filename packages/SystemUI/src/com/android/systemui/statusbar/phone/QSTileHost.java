b83790d07309c50da3fb9f8324f564224df20c70
Roman Birg <roman@cyngn.com>
2015-01-22 11:21:56 -0800

    SystemUI: add Profiles tile
    Change-Id: I9fd2c807272f73159d13fbe23404568e54c519bf
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.ProfilesTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
@@ -282,6 +283,8 @@ public class QSTileHost implements QSTile.Host {
                 return new DdsTile(this);
             case QSConstants.TILE_APN:
                 return new ApnTile(this);
+            case QSConstants.TILE_PROFILES:
+                return new ProfilesTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
