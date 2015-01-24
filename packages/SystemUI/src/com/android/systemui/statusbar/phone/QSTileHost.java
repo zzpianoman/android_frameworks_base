187c97fe9339e649220a03003469d727b4fb4c51
Danesh M <daneshm90@gmail.com>
2015-01-23 18:59:21 -0800

    Add Timeout QS Tile
    * This is the second standalone tile I add (it doesn't need CM's implementation, works on top of AOSP).
    * Adapted for marshmallow by @KreAch3R
    * SystemUI Tuner configurable
    * LocationDetailAdapter --> TimeoutDetailAdapter
    
    Credits to the original authors of the commits below, plus zephiK, Danesh
    
    This is a combination of 4 commits.
    SystemUI : Add screen timeout tile
    
    Change-Id: I0854e940f9604dee2fff5093e688ef3a17b258a9
    
    Update screen timeout tile
    Credits to romanbb - https://github.com/zephiK/android_frameworks_base/commit/97ffbacac96e64198291a75dcde41bdd3f339ef9
    
    Update screen timeout tile part 2
    Credits to mikeNG - https://github.com/zephiK/android_frameworks_base/commit/3e3ed753af5a3b500f3636e0f2d569d47ccd6e8c
    
    qs: support for all timeout value in case set by user apps
    
    fix bug: after timeout is set by apps like Tasker to values not in the
    known options list (e.g. 23 Hours), changing the value no longer
    update tile title, it stay as "23 Hours"
    
    Change-Id: Ica11d473de6935bac02ba30557ac1b1e30d3211b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -47,6 +47,7 @@ import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.VolumeTile;
 import com.android.systemui.qs.tiles.SyncTile;
+import com.android.systemui.qs.tiles.ScreenTimeoutTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.statusbar.policy.BluetoothController;
 import com.android.systemui.statusbar.policy.CastController;
@@ -278,6 +279,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("compass")) return new CompassTile(this);
         else if (tileSpec.equals("sync")) return new SyncTile(this);
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(this);
+        else if (tileSpec.equals("timeout")) return new ScreenTimeoutTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
