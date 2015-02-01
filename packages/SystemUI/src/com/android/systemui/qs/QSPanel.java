ba64f7071b94376ca9efdcb6b28cddbc04db7cfb
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-01 06:41:03 +0000

    Option to show four tiles per row
    This also reduces the size of the ripple when using 4 tiles per row
    
    -Moved Settings switch to SystemUI Tuner
    
    Change-Id: Id61d6f2b113feabda46f464d0eda51c811226dd8

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -67,6 +67,7 @@ public class QSPanel extends ViewGroup {
     private final H mHandler = new H();
 
     private int mColumns;
+    private int mNumberOfColumns;
     private int mCellWidth;
     private int mCellHeight;
     private int mLargeCellWidth;
@@ -159,6 +160,22 @@ public class QSPanel extends ViewGroup {
         }
     }
 
+    /**
+     * Use three or four columns.
+     */
+    private int useFourColumns() {
+        final Resources res = mContext.getResources();
+        boolean shouldUseFourColumns = Settings.System.getInt(
+            mContext.getContentResolver(), Settings.System.QS_USE_FOUR_COLUMNS,
+                0) == 1;
+        if (shouldUseFourColumns) {
+            mNumberOfColumns = 4;
+        } else {
+            mNumberOfColumns = res.getInteger(R.integer.quick_settings_num_columns);
+        }
+        return mNumberOfColumns;
+    }
+
     private void updateDetailText() {
         mDetailDoneButton.setText(R.string.quick_settings_done);
         mDetailSettingsButton.setText(R.string.quick_settings_more_settings);
@@ -195,7 +212,7 @@ public class QSPanel extends ViewGroup {
 	        numColumns = res.getInteger(R.integer.quick_settings_num_columns_landscape);
             }
         }
-        final int columns = Math.max(1, numColumns);
+        final int columns = Math.max(1, useFourColumns());
         mCellHeight = res.getDimensionPixelSize(R.dimen.qs_tile_height);
         mCellWidth = (int)(mCellHeight * TILE_ASPECT);
         mLargeCellHeight = res.getDimensionPixelSize(R.dimen.qs_dual_tile_height);
