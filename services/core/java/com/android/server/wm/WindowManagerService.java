0da392532a643486402effbb0ef3202c67da421e
Roman Birg <roman@cyngn.com>
2015-05-21 06:22:24 -0700

    unbreak density scaling
    - Only apply density scaling to built in screens so we don't affect
    overlays.
    
    - Only apply density scaling for activities which fall under the
      'default' compatibility bucket. Some apps may  have different
      compatibility info setup (e.g. scaling) and we do not
      need to apply the user density to this activity.
    
    Change-Id: I0b4849141cf081694aae5668ae60764e70be7db1
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Density scaling: scale compatibility apps, wm command support
    
    Density scaling should work across all apps and different density
    configurations. It should also pass CTS.
    
    - force compatibility density if calculated compatibility density is
      different than what the user has requested
    
    - hook in "wm density" support, should behave the same as setting the
      density from Settings
    
    - clean up some logic
    
    Change-Id: I52c7c2ca200b78f9a5ee9170871ca62e1cab3bcb
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Fix typo in density assignment.
    
    Change-Id: I35b3860a4991ebaee70c91f3878d63b8f28d7fce
    
    Density scaling: return device's default density
    
    When an application requests the Display's display metrics, always
    override the returned densityDpi to be the device's original density to
    ensure compatibility.
    
    Change-Id: I465d53bc189d4596aba0269f124ddc9d0d8097da
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    return default density only for primary display
    
    Change-Id: I70b35a7f4ba77d7c5744fdcb6c19ef78de3dcb15
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    density: do not apply to virtual displays
    
    Check the incoming configuration density to ensure that it is the same
    as the device's default density before forcing preferred values.
    
    Only apply display metrics if the display is not a presentation.
    
    Ref: CYNGNOS-338
    Change-Id: I437ac8cf45f1a6c568707e0733364b2495791d8a
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -298,6 +298,7 @@ public class WindowManagerService extends IWindowManager.Stub
     private static final String SYSTEM_SECURE = "ro.secure";
     private static final String SYSTEM_DEBUGGABLE = "ro.debuggable";
 
+    private static final String PERSIST_SYS_LCD_DENSITY = "persist.sys.lcd_density";
     private static final String DENSITY_OVERRIDE = "ro.config.density_override";
     private static final String SIZE_OVERRIDE = "ro.config.size_override";
 
@@ -8558,6 +8559,9 @@ public class WindowManagerService extends IWindowManager.Stub
 
     @Override
     public int getInitialDisplayDensity(int displayId) {
+        if (displayId == Display.DEFAULT_DISPLAY) {
+            return DisplayMetrics.DENSITY_DEVICE_DEFAULT;
+        }
         synchronized (mWindowMap) {
             final DisplayContent displayContent = getDisplayContentLocked(displayId);
             if (displayContent != null && displayContent.hasAccess(Binder.getCallingUid())) {
@@ -8571,6 +8575,9 @@ public class WindowManagerService extends IWindowManager.Stub
 
     @Override
     public int getBaseDisplayDensity(int displayId) {
+        if (displayId == Display.DEFAULT_DISPLAY) {
+            return DisplayMetrics.DENSITY_PREFERRED;
+        }
         synchronized (mWindowMap) {
             final DisplayContent displayContent = getDisplayContentLocked(displayId);
             if (displayContent != null && displayContent.hasAccess(Binder.getCallingUid())) {
@@ -8598,6 +8605,7 @@ public class WindowManagerService extends IWindowManager.Stub
             synchronized(mWindowMap) {
                 final DisplayContent displayContent = getDisplayContentLocked(displayId);
                 if (displayContent != null) {
+                    SystemProperties.set(PERSIST_SYS_LCD_DENSITY, Integer.toString(density));
                     setForcedDisplayDensityLocked(displayContent, density);
                     Settings.Global.putString(mContext.getContentResolver(),
                             Settings.Global.DISPLAY_DENSITY_FORCED, Integer.toString(density));
@@ -8605,6 +8613,10 @@ public class WindowManagerService extends IWindowManager.Stub
             }
         } finally {
             Binder.restoreCallingIdentity(ident);
+        }
+        try {
+            ActivityManagerNative.getDefault().restart();
+        } catch (RemoteException e) {
         }
     }
 
@@ -8634,6 +8646,7 @@ public class WindowManagerService extends IWindowManager.Stub
             synchronized(mWindowMap) {
                 final DisplayContent displayContent = getDisplayContentLocked(displayId);
                 if (displayContent != null) {
+                    SystemProperties.set(PERSIST_SYS_LCD_DENSITY, null);
                     setForcedDisplayDensityLocked(displayContent,
                             displayContent.mInitialDisplayDensity);
                     Settings.Global.putString(mContext.getContentResolver(),
@@ -8642,6 +8655,10 @@ public class WindowManagerService extends IWindowManager.Stub
             }
         } finally {
             Binder.restoreCallingIdentity(ident);
+        }
+        try {
+            ActivityManagerNative.getDefault().restart();
+        } catch (RemoteException e) {
         }
     }
 
