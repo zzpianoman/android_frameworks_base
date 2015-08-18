e3b599ebca0401a0feef6eb37e5095ebf1e21e68
Justin <zzpianoman@gmail.com>
2015-08-17 21:05:13 -0400

    ActivityManager: Config prop to hide isLowRamDevice()=true from apps
    Selectively force isLowRamDevice() to return "false" for apps specified
    in comma separated list. This will allow certain apps to operate with
    all features while the low_ram flag is set to true, i.e. photosphere
    and panorama for GoogleCamera.
    
    example:
    
    ro.config.lowram_hide_apps=com.google.android.GoogleCamera

--- core/java/android/app/ActivityManager.java
+++ core/java/android/app/ActivityManager.java
@@ -63,6 +63,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -467,7 +469,13 @@ public class ActivityManager {
      * off certain features that require more RAM.
      */
     public boolean isLowRamDevice() {
-        return isLowRamDeviceStatic();
+        String order = SystemProperties.get("ro.config.lowram_hide_apps", "none");
+        List<String> mPackageNames = new LinkedList<String>(Arrays.asList(order.split(",")));
+        if (mPackageNames.contains(mContext.getPackageName())) {
+            return false;
+        } else {
+            return isLowRamDeviceStatic();
+        }
     }
 
     /** @hide */
