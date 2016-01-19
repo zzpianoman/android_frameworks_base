4d3ccf757a5f102db468166ced3a535849ac45bb
Jorge Martin Espinosa <jorgemartinespinosa@gmail.com>
2016-01-19 14:58:41 +0000

    Add option to turn off main tiles
    -Moved settings switch to SystemUI Tuner
    
    Change-Id: I385d0ba8a1b188314a322815f23353cf632285d9

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -51,11 +51,6 @@ public class BluetoothTile extends QSTile<QSTile.BooleanState>  {
     }
 
     @Override
-    public boolean supportsDualTargets() {
-        return true;
-    }
-
-    @Override
     public DetailAdapter getDetailAdapter() {
         return mDetailAdapter;
     }
